82170ba28d70b71e95ff274334d4ec058fdac6b5
aviavissar <aviavissar@gmail.com>
2020-08-03 15:11:10 +0300

    starting

---
+++ src/service/fetchData.js
@@ -0,0 +1,22 @@
+export const getData = async () => {
+  try {
+    const response = await fetch(
+      "https://guard.io/v2/hiring/fe/breaches?offset=0",
+      {
+        method: "GET",
+        mode: "cors", 
+        credentials: "same-origin", 
+        headers: {
+          "Content-Type": "application/json",
+          "X-Best-Pokemon": "Pikachu",
+        },
+        referrerPolicy: "no-referrer",
+      }
+    );
+    const res_obj = await response.json();
+
+    return res_obj.items; // parses JSON response into native JavaScript objects
+  } catch (error) {
+    console.log("client error" + error);
+  }
+};
